cf6c39f34248769973887ea756ba7d1940ea4d86
wallaceyuan <1272864289@qq.com>
2016-06-28 22:53:17 +0800

    wechat

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Yuan on 2016/6/28.
+ */
+'use strict'
+
+var Koa = require('koa');
+var sha1 = require('sha1');
+
+var config = {
+    wechat:{
+        appID : 'wx0527f87ddccbfe13',
+        appSecret:'ba2de824eed86e2000980b68e383d9c9',
+        token:'TfxtCvw686cEtzB7o7fk'
+    }
+}
+
+var app = new Koa();
+
+app.use(function *(res,req,next){
+    console.log(this.query);
+});
+
+app.listen(1234);
+
+console.log('Listening');
+
+
